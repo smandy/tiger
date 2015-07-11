75ad8b2eb55883ff25d67f6a3bbd31447871a2a5
smandy <andyrsmith@googlemail.com>
2015-07-11 13:20:03 +0100

    Add discovery python client with some async kewlness.

---
+++ python/discovery.py
@@ -0,0 +1,78 @@
+import Ice
+
+Ice.loadSlice('../slice/Foo.ice')
+import argo
+
+communicator = Ice.initialize( [ '--Ice.Config=locator.properties'])
+cppPrx = communicator.stringToProxy('foo@SimpleCppApp')
+cppFoo = argo.FooPrx.checkedCast(cppPrx)
+
+print "Doit      -> %s" % cppFoo.doit()
+print "DoitAgain -> %s" % cppFoo.doitAgain()
+
+pyPrx = communicator.stringToProxy('foo@SimpleApp')
+pyFoo = argo.FooPrx.checkedCast(pyPrx)
+
+print "Doit      -> %s" % pyFoo.doit()
+print "DoitAgain -> %s" % pyFoo.doitAgain()
+
+scalaPrx = communicator.stringToProxy('foo@SimpleJavaApp')
+scalaFoo = argo.FooPrx.checkedCast(scalaPrx).ice_timeout(5000)
+
+print "Doit      -> %s" % scalaFoo.begin_doit()
+print "DoitAgain -> %s" % scalaFoo.begin_doitAgain()
+
+
+proxies = [ cppFoo, pyFoo, scalaFoo ]
+
+# asyncinvocations.
+invocations = []
+invocations += [ (x.end_doit      , x.begin_doit() )      for x in proxies ]
+invocations += [ (x.end_doitAgain , x.begin_doitAgain() ) for x in proxies ]
+
+for getter, invocation in invocations:
+    print "%s -> %s" % (getter, getter(invocation))
+
+import functools
+
+from pprint import pprint as pp
+results = []
+def receive( prx, method, resp):
+    print "Woot %s %s %s" % (prx, method, resp)
+    results.append( (prx, method, resp) )
+
+def myComplete():
+    print "All Done"
+    pp(results)
+    communicator.shutdown()
+
+class Collector:
+    def __init__(self, onComplete):
+        self.methods = {}
+        self.counter = 0
+        self.onComplete = onComplete
+    
+    def curry( self, f, *argx ):
+        id = self.counter
+        self.counter += 1
+        def ret(*argy):
+            #print "Id is %s" % id
+            del self.methods[id]
+            newArgs = argx + argy
+            r2 = f( *newArgs )
+            if not(self.methods):
+                #print "empty?"
+                self.onComplete()
+            return r2
+        self.methods[id] = ret
+        return ret
+
+col   = Collector(myComplete)
+curry = col.curry
+
+def myExcept(exc):
+    print "Exception"
+
+for x in proxies:
+    x.begin_doit(      _response = curry( receive, x, 'doit'      ) , _ex = myExcept )
+    x.begin_doitAgain( _response = curry( receive, x, 'doitAgain' ) , _ex = myExcept )

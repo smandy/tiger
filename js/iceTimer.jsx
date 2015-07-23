1f1eff8d1e4f5d0bc50be2f686c6ef6cec6256bf
Andy Smith <andyrsmith@googlemail.com>
2015-07-23 18:26:01 +0100

    Update to iceTimer.jsx

--- js/iceTimer.jsx
+++ js/iceTimer.jsx
@@ -1,66 +1,38 @@
-
 var iid = new Ice.InitializationData();
 iid.properties = Ice.createProperties();
 iid.properties.setProperty("Ice.Default.Locator", "IceGrid/Locator:ws -h localhost -p 4063");
-//iid.properties.setProperty("Ice.Trace.Protocol", "1");
 var communicator = Ice.initialize([], iid);
 
 var FooCaller = React.createClass({
-    getInitialState: function(){
-        //console.log("StrProxy is " + this.props.strProxy);
+    getInitialState: function() {
         var myPrx = communicator.stringToProxy(this.props.strProxy);
         var prx = argo.FooPrx.uncheckedCast(myPrx);
-        // console.log("Checked is ");
-        // console.log(prx);
-        return { proxy : prx , counter : 0 };
+        return { proxy : prx ,
+                 strProxy : this.props.strProxy,
+                 counter : 0 };
     },
-
     componentDidMount: function(){
-        // componentDidMount is called by react when the component 
-        // has been rendered on the page. We can set the interval here:
         this.timer = setInterval(this.ping, 2000);
     },
-
     componentWillUnmount: function(){
-
-        // This method is called immediately before the component is removed
-        // from the page and destroyed. We can clear the interval here:
-
         clearInterval(this.timer);
     },
-
-
     mySetState : function(x) {
-        //console.log("setstate");
-        //console.log(x);
         this.setState(x);
-        
     },
-    
     ping : function(){
-        // This function is called every 50 ms. It updates the 
-        // elapsed counter. Calling setState causes the component to be re-rendered
-        //this.setState({elapsed: new Date() - this.props.start});
-        //console.log("Proxy is ");
-        //console.log( this.state.proxy);
         var outer = this;
         Ice.Promise.try( function() {
             outer.state.proxy.doit().then(
                 function(ret) {
-                    //console.log("Success " + ret);
-                    //console.log(outer);
-                    //outer.mySetState( { foo : "Bar"} );
                     var newState = { status : "Success " + ret,
                                      proxy  : outer.state.proxy ,
+                                     strProxy : outer.state.strProxy,
                                      counter : outer.state.counter + 1,
                                      date : new Date() };
                     outer.mySetState(newState);
-                    //console.log(newState);
                 },
                 function (ex) {
-                    //console.log("Failure ");
-                    //console.log(outer);
-                    //outer.mySetState( { foo : "Bar"} );
                     outer.mySetState( { status : "Failure" + ret,
                                         proxy  : outer.state.proxy } );
                 }
@@ -69,19 +41,12 @@ var FooCaller = React.createClass({
             console.log(ex);
         } );
     },
-
     render: function() {
-        //console.log( "Rendering");
-        //console.log( this.state);
-        //var secs = this.state.status;
-        //console.log(secs);
         var d = new Date();
-        return <p>This example was started <b>{this.state.status}</b> <b>{this.state.counter}</b> seconds ago. {d.toString} </p>;
+        return <p>{this.state.strProxy} ret="{this.state.status}" {this.state.counter}  {d.toString()}</p>;
     }
 });
 
-
-//var proxies = ["foo@SimpleCppApp","foo@SimpleJavaApp","foo@SimpleApp"];
 React.render(
         <TimerExample start={Date.now()} />,
     document.getElementById("timer")

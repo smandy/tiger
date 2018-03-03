e444d5a4a5f83fbd82b219ad25457d0b401ea679
Andy Smith <andyrsmith@gmail.com>
2018-03-03 14:36:04 +0000

    More renames.

--- d/callD.py
+++ d/callD.py
@@ -1,8 +1,7 @@
-
 import Ice
 
 Ice.loadSlice( '../slice/Foo.ice')
 import argo
-
 communicator = Ice.initialize()
-fooPrx = argo.FooPrx.uncheckedCast( communicator.stringToProxy( 'foo:tcp -p 1099 -h ritz') )
+fooPrx = argo.FooPrx.uncheckedCast( communicator.stringToProxy('foo:tcp -p 1099 -h ritz'))
+print( [ getattr(fooPrx, x)() for x in ['doit', 'doitAgain'] ] )

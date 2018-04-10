7f231247998a4db82610b60586ab427f40fdb560
Andy Smith <andyrsmith@gmail.com>
2018-04-11 00:27:01 +0100

    Tidy up clojure listener.

--- clj/src/clj/tickListener.clj
+++ clj/src/clj/tickListener.clj
@@ -9,25 +9,21 @@
 (import argo.TickListenerPrx)
 (import argo.TickListener))
 
+                ;;(.setProperty "Ice.Default.EndpointSelection" "Ordered")
+                ;;(.setProperty "Ice.Warn.Connections" "0")
+                ;;(.setProperty "Ice.Trace.Protocol"  "0"))
+                ;;(.setProperty "IceDiscovery.DomainId"  "argo"))
+
+
 (defn communicator []
   (let [iid (new InitializationData)
         props (doto (Util/createProperties)
                 (.setProperty "Ice.Plugin.IceDiscovery"  "IceDiccovery:com.zeroc.IceDiscovery.PluginFactory")
-                ;;(.setProperty "Ice.Default.EndpointSelection" "Ordered")
-                ;;(.setProperty "Ice.Warn.Connections" "0")
-                (.setProperty "Ice.Trace.Network"  "1")
-                ;;(.setProperty "Ice.Trace.Protocol"  "0"))
-                ;;(.setProperty "IceDiscovery.DomainId"  "argo"))
-                )] 
+                (.setProperty "Ice.Trace.Network"  "1"))] 
     (set! (. iid properties) props)
     (Util/initialize iid)))
 
-
 (defn makeuuid [] (.toString (java.util.UUID/randomUUID)) )
-
-;; (defn adapter []
-;;   (let [ comm (communicator) ]
-;;     (.createObjectAdapterWithEndpoints comm (makeuuid) "tcp")))
 
 (defn adapter [comm]
     (.createObjectAdapterWithEndpoints comm (makeuuid) "tcp"))
@@ -41,17 +37,13 @@
 
 (defn makeimpl [] (reify argo.TickListener
                     (onTick [this ticks current]
-                      (printf "Got a tick woot %s\n" ticks)
-                      )
-                    ))
+                      (printf "Got a tick woot %s\n" ticks))))
 
 (defn doit []
-  (let [
-        comm (communicator)
+  (let [comm (communicator)
         adp (adapter comm)
         my_prx (argo.TickListenerPrx/checkedCast (.addWithUUID adp (makeimpl)))
-        prx (argo.TickerPlantPrx/checkedCast (.stringToProxy comm "plant"))
-        ]
+        prx (argo.TickerPlantPrx/checkedCast (.stringToProxy comm "plant"))]
     (.activate adp)
     (.subscribe prx my_prx)))
 

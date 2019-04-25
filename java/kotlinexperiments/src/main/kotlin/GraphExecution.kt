e0a7a95c5b9d205ea9b0c5e3c7fac41f3f3702b0
Andy Smith <andyrsmith@gmail.com>
2019-04-25 03:31:40 +0100

    Trying to get grpc + maven playing together.

--- java/kotlinexperiments/src/main/kotlin/GraphExecution.kt
+++ java/kotlinexperiments/src/main/kotlin/GraphExecution.kt
@@ -2,6 +2,8 @@ package foo
 
 class Graph {
     inner class Node(val name : String) {
+        var valid : Boolean = false
+
         override fun toString() = "Node($name)"
 
         val children: List<Node>
@@ -9,15 +11,33 @@ class Graph {
                     .filter { it.from.name == name }
                     .map { it.to }
 
-        fun canFind(other: String, depth: Int = 0): Int? =
-                if (other == name) {
-                    depth
-                } else {
-                    children
-                            .mapNotNull { it.canFind(other) }
-                            .min()
-                            ?.let { it + 1 }
-                }
+        fun canFind(other: String, depth: Int = 0): Int? {
+            println("${this}.canFind $other $depth")
+            return if (other == name) {
+                depth
+            } else {
+                children
+                        .mapNotNull { it.canFind(other) }
+                        .min()
+                        ?.let { it + 1 }
+            }
+        }
+
+        fun canFind2(other : String, depth : Int = 0, visited : MutableMap<Node,List<Int>> = mutableMapOf()) : MutableMap<Node,List<Int>> {
+            println("${this}.canFind2( $other, $depth, $visited")
+            val otherNode = getNode(other)
+            if ( other == name) {
+                visited[otherNode] = visited[otherNode]?.let { it + depth } ?: listOf(depth)
+            } else {
+                children.forEach { it.canFind2(other, depth + 1, visited)}
+            }
+            return visited
+        }
+
+        fun invalidate() {
+            valid = false
+            children.forEach { it.invalidate() }
+        }
     }
 
     inner class Edge(val from : Graph.Node, val to : Graph.Node)
@@ -49,17 +69,26 @@ class Graph {
         """.trimMargin()
 }
 
-
 fun main(args: Array<String>) {
     val g = Graph()
     g.getOrCreateEdge("a", "b")
     g.getOrCreateEdge("b", "c")
     g.getOrCreateEdge("c", "d")
     g.getOrCreateEdge("a", "c")
-    println( g.getNode("a").canFind("a"))
-    println( g.getNode("a").canFind("b"))
-    println( g.getNode("a").canFind("c"))
-    println( g.getNode("a").canFind("d"))
-    println( g.getNode("a").canFind("e"))
-    println(g)
+
+/*
+    println(g.getNode("a").canFind("a"))
+    println(g.getNode("a").canFind("b"))
+    println(g.getNode("a").canFind("c"))
+    println(g.getNode("a").canFind("d"))
+    println(g.getNode("a").canFind("e"))
+*/
+
+    val g2 = Graph()
+    g.getOrCreateEdge("a", "z")
+    g.getOrCreateEdge("a", "b")
+    g.getOrCreateEdge("b", "c")
+    g.getOrCreateEdge("c", "d")
+    g.getOrCreateEdge("d", "z")
+    println(g2.getNode("a").canFind2("z"))
 }
